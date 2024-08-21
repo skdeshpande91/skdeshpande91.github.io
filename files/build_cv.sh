c8b5cd5904012e9f3fe1adcea0cbafe330844a4e
Sameer Deshpande <sameerd@alum.mit.edu>
2024-08-21 07:31:24 -0500

    New cv format

---
+++ files/build_cv.sh
@@ -0,0 +1,13 @@
+pdflatex Deshpande_cv_new.tex
+biber Deshpande_cv_new
+pdflatex Deshpande_cv_new.tex
+pdflatex Deshpande_cv_new.tex
+
+rm *.aux
+rm *.blg
+rm *.bcf
+rm *.fdb_latexmk
+rm *.fls
+rm *.log
+rm *.xml
+rm *.synctex.gz
